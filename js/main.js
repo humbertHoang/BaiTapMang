cc63caf9d9972e3f1908c97e168a6289e26a8c2c
csphat001 <csphat1@gmail.com>
2024-10-06 03:27:36 +0700

    update toan bo bai tap

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ const setValue = (id, value) => {
   getElement(id).innerHTML = value;
 };
 
-const formatArray = (arr) => {
+const formatArray = (arr, id = "currArray") => {
   const formatArray = arr.join(" | ");
-  setValue("currArray", formatArray);
+  setValue(id, formatArray);
 };
 const addToArray = () => {
   const value = getValue("arrvalueInput");
@@ -24,7 +24,6 @@ const addToArray = () => {
 const deleteFromArray = () => {
   if (currArray.length > 0) {
     currArray.pop();
-    console.log(currArray);
     formatArray(currArray);
   } else {
     alert("Mảng rỗng");
@@ -48,6 +47,117 @@ getElement("sumarrBtn").onclick = () => {
   }
   setValue("sumArray", sum);
 };
+//Bài 2
+getElement("countposnumBtn").onclick = () => {
+  let count = 0;
+  for (let i = 0; i < currArray.length; i++) {
+    if (currArray[i] > 0) {
+      count++;
+    }
+  }
+  setValue("countposnumArray", count);
+};
+//Bài 3
+getElement("findminBtn").onclick = () => {
+  let min = currArray[0];
+  for (let i = 0; i < currArray.length; i++) {
+    if (currArray[i] < min) {
+      min = currArray[i];
+    }
+  }
+  setValue("findminArray", min);
+};
+//Bài 4
+getElement("findposminBtn").onclick = () => {
+  let min = currArray[0];
+  for (let i = 0; i < currArray.length; i++) {
+    if (currArray[i] < min && currArray[i] > 0) {
+      min = currArray[i];
+    }
+  }
+  setValue("findposminArray", min);
+};
+//Bài 5
+getElement("evennumBtn").onclick = () => {
+  let evenArray = null;
+  for (let i = 0; i < currArray.length; i++) {
+    if (currArray[i] % 2 === 0) {
+      evenArray = currArray[i];
+    }
+  }
+  if (evenArray === null) {
+    setValue("evennumArray", "-1");
+  } else {
+    setValue("evennumArray", evenArray);
+  }
+};
+//Bài 6
+getElement("switchnumBtn").onclick = () => {
+  const firstIndex = getValue("firstindexInput");
+  const secondIndex = getValue("secondindexInput");
+  let copyArray = [...currArray];
+  console.log(copyArray);
+  if (firstIndex && secondIndex) {
+    const temp = copyArray[firstIndex];
+    copyArray[firstIndex] = copyArray[secondIndex];
+    copyArray[secondIndex] = temp;
+    formatArray(copyArray, "switchnumArray");
+  } else {
+    alert("Vui long nhap index");
+  }
+};
+//Bài 7
+getElement("sortingBtn").onclick = () => {
+  let copyArray = [...currArray];
+  copyArray.sort((a, b) => a - b);
+  formatArray(copyArray, "sortingArray");
+};
 
+//Bài 8
+const checkPrime = (n) => {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+getElement("firstprimenumBtn").onclick = () => {
+  let firstPrimeNum = -1;
+  for (let i = 0; i < currArray.length; i++) {
+    if (checkPrime(currArray[i])) {
+      firstPrimeNum = currArray[i];
+      break;
+    }
+  }
+  setValue("firstprimenumArray", firstPrimeNum);
+};
+// Bài 9
+getElement("isintBtn").onclick = () => {
+  let countInt = 0;
+  for (let i = 0; i < currArray.length; i++) {
+    if (Number.isInteger(currArray[i])) {
+      countInt++;
+    }
+  }
+  setValue("isintArray", countInt);
+};
+
+//Bai 10
+getElement("compareBtn").onclick = () => {
+  let posNumber = 0;
+  let negNumber = 0;
+  for (let i = 0; i < currArray.length; i++) {
+    currArray[i] > 0 ? posNumber++ : currArray[i] < 0 && negNumber++;
+  }
+  posNumber > negNumber
+    ? setValue("compareArray", "Số dương > Số âm")
+    : posNumber < negNumber
+    ? setValue("compareArray", "Số dương < Số âm")
+    : setValue("compareArray", "Số dương = Số âm");
+};
 getElement("addBtn").onclick = addToArray;
 getElement("delBtn").onclick = deleteFromArray;
